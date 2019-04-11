b09b847f92a87898218a7a3ba0a6d2de57673f9e
Adam Kapica <adam.kapica@gmail.com>
2019-04-11 20:30:06 +0200

    Added multithreads.

--- cpp_client/src/main.cpp
+++ cpp_client/src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <pthread.h>
 #include "client/client.h"
+
+pthread_t sendThread, readThread;
+Client* client;
+
+void* send(void* arg){
+    char* message = new char[MAXMESSAGESIZE];
+    for(;;){
+        std::cin >> message;
+        client->send(message);
+    }
+}
+
+void* read(void* arg){
+    const char* message;
+    for(;;){
+        message = client->receive();
+        std::cout << message;
+    }
+}
 
 int main(int argc, char** argv) {
 
@@ -8,10 +28,14 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    Client* client = new Client();
+    client = new Client();
     client->run("localhost", 44444);
 
-    const char* result = client->receive();
-    std::cout<<result;
+    pthread_create(&sendThread, NULL, &send, NULL);
+    pthread_create(&readThread, NULL, &read, NULL);
+
+    pthread_join(sendThread, NULL);
+    pthread_join(readThread, NULL);
+
     return 0;
 }
