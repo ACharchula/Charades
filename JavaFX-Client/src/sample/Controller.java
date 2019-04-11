b74caa7cb7143f63178028bdd641890daeb564ef
tokerson <kuba.tokarzewski@gmail.com>
2019-04-11 23:03:16 +0200

    refactored writing message thread

--- JavaFX-Client/src/sample/Controller.java
+++ JavaFX-Client/src/sample/Controller.java
@@ -9,16 +9,14 @@ import sample.Model.Message;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Controller {
 
-    private Socket socket;
     private PrintWriter out;
-    private DataInputStream in;
-
-    private ArrayList<Message> messages = new ArrayList<>();
+    private BufferedReader in;
 
     @FXML
     private ListView messagesBox;
@@ -27,31 +25,36 @@ public class Controller {
     private TextField messageField;
 
     public Controller() {
-
         startConnection("127.0.0.1", 44444);
-        startChatReceiverTask();
     }
 
-    private void startChatReceiverTask(){
-        Runnable task = this::runTask;
+    private void startConnection(String ip, int port) {
+        try {
+            Socket socket = new Socket(ip, port);
+            socket.setTcpNoDelay(true);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startChatReceiverTask() {
+        Runnable task = this::runChatReceiverTask;
 
         Thread backgroundThread = new Thread(task);
         backgroundThread.setDaemon(true);
         backgroundThread.start();
     }
 
-    private void runTask(){
-        try{
-            while(true){
-                byte[] lengthBytes = new byte[4];
-                in.readFully(lengthBytes, 0, 4);
-                System.out.println(Arrays.toString(lengthBytes));
-                System.out.println(new String(lengthBytes));
-                int length = Integer.parseInt(new String(lengthBytes));
+    private void runChatReceiverTask() {
+        try {
+            while (true) {
+                int length = Integer.parseInt(in.readLine());
                 System.out.println(length);
-                if(length > 0){
-                    byte[] message = new byte[length];
-                    in.readFully(message, 0, length);
+                if (length > 0) {
+                    char[] message = new char[length];
+                    in.read(message, 0, length);
                     System.out.println(new String(message));
                     in.read(); // eat new line char
                     Platform.runLater(() -> messagesBox.getItems().add(messagesBox.getItems().size(), new String(message)));
@@ -62,24 +65,35 @@ public class Controller {
         }
     }
 
-    @FXML
-    public void initialize() {
+    private void startChatWriterTask() {
+        Runnable task = this::runChatWriterTask;
 
-        System.out.println("initialize");
+        Thread backgroundThread = new Thread(task);
+        backgroundThread.setDaemon(true);
+        backgroundThread.start();
+    }
+
+    private void runChatWriterTask() {
 
         messageField.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) {
-                addMessage(messageField.getText());
+                addMessage();
             }
         });
+
     }
 
-    private void addMessage(String text) {
+    @FXML
+    public void initialize() {
+        startChatReceiverTask();
+        startChatWriterTask();
+    }
+
+    private void addMessage() {
+        String text = messageField.getText();
         if (text.length() > 0) {
             Message message = new Message("Tokarz", text);
-            messages.add(message);
-            WritingChatMessageThread thread = new WritingChatMessageThread(message.getContent());
-            thread.start();
+            sendMessage(message.getContent());
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append(message.getAuthor() + ": " + message.getContent());
             messagesBox.getItems().add(messagesBox.getItems().size(), stringBuilder.toString());
@@ -87,37 +101,11 @@ public class Controller {
         }
     }
 
-    public void startConnection(String ip, int port) {
-        try {
-            socket = new Socket(ip, port);
-            socket.setTcpNoDelay(true);
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new DataInputStream(socket.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void sendMessage(String message) {
+        out.println(message.length());
+        out.println(message);
     }
 
-    class WritingChatMessageThread extends Thread {
-        private String message;
-
-        public WritingChatMessageThread(String message) {
-            this.message = message;
-        }
-
-        public void run() {
-            try {
-                out.println(message.length());
-                out.println(message);
-
-            } catch (Exception e) {
-                System.out.println("Exception in writer is caught");
-            } finally {
-                Thread.currentThread().interrupt();//preserve the message
-                return;//Stop doing whatever I am doing and terminate
-            }
-        }
-    }
 
 
 }
