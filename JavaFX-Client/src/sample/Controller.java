935f635a50ca98df624e5a817055f398307de7d5
tokerson <kuba.tokarzewski@gmail.com>
2019-04-12 00:27:51 +0200

    char working

--- JavaFX-Client/src/sample/Controller.java
+++ JavaFX-Client/src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -9,9 +10,7 @@ import sample.Model.Message;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.Buffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Controller {
 
@@ -24,8 +23,29 @@ public class Controller {
     @FXML
     private TextField messageField;
 
+    @FXML
+    public void initialize() {
+        startChatReceiverTask();
+        startChatWriterTask();
+        messagesBox.setCellFactory(param -> new ListCell<String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setGraphic(null);
+                    setText(null);
+                    // other stuff to do...
+                } else {
+                    setPrefWidth(messagesBox.getWidth());
+                    setWrapText(true);
+                    setText(item);
+                }
+            }
+        });
+    }
+
     public Controller() {
-        startConnection("127.0.0.1", 44444);
+        startConnection("127.0.0.1", 45655);
     }
 
     private void startConnection(String ip, int port) {
@@ -34,9 +54,15 @@ public class Controller {
             socket.setTcpNoDelay(true);
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            sendWelcomePackage();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void sendWelcomePackage() {
+        String welcomePackage = "HELLO\ntokarz";
+        out.println(welcomePackage);
     }
 
     private void startChatReceiverTask() {
@@ -50,19 +76,33 @@ public class Controller {
     private void runChatReceiverTask() {
         try {
             while (true) {
-                int length = Integer.parseInt(in.readLine());
-                System.out.println(length);
-                if (length > 0) {
-                    char[] message = new char[length];
-                    in.read(message, 0, length);
-                    System.out.println(new String(message));
-                    in.read(); // eat new line char
-                    Platform.runLater(() -> messagesBox.getItems().add(messagesBox.getItems().size(), new String(message)));
+                String messageType = in.readLine();
+                System.out.println(messageType);
+                if (!messageType.equals("WELCOME")) {
+                    String user = in.readLine();
+                    System.out.println(user);
+                    int length = Integer.parseInt(in.readLine());
+                    System.out.println(length);
+                    if (length > 0) {
+                        char[] message = new char[length];
+                        in.read(message, 0, length);
+                        System.out.println(new String(message));
+                        Message msg = new Message(user, new String(message));
+                        Platform.runLater(() -> {
+                            messagesBox.getItems().add(messagesBox.getItems().size(), formatMessageToShow(msg));
+                            messagesBox.scrollTo(messagesBox.getItems().size() - 1);
+                        });
+                    }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String formatMessageToShow(Message message) {
+        StringBuilder stringBuilder = new StringBuilder(message.getAuthor() + ": " + message.getContent());
+        return stringBuilder.toString();
     }
 
     private void startChatWriterTask() {
@@ -83,29 +123,24 @@ public class Controller {
 
     }
 
-    @FXML
-    public void initialize() {
-        startChatReceiverTask();
-        startChatWriterTask();
-    }
 
     private void addMessage() {
         String text = messageField.getText();
         if (text.length() > 0) {
-            Message message = new Message("Tokarz", text);
+            Message message = new Message("Ja", text);
             sendMessage(message.getContent());
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(message.getAuthor() + ": " + message.getContent());
-            messagesBox.getItems().add(messagesBox.getItems().size(), stringBuilder.toString());
-            messageField.clear();
+            Platform.runLater( () -> {
+                messagesBox.getItems().add(messagesBox.getItems().size(), formatMessageToShow(message));
+                messageField.clear();
+            } );
         }
     }
 
     private void sendMessage(String message) {
+        out.println("SEND_MESSAGE");
         out.println(message.length());
         out.println(message);
     }
-
 
 
 }
--- JavaFX-Client/src/sample/Main.java
+++ JavaFX-Client/src/sample/Main.java
@@ -11,8 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        System.out.println("Hello world");
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Charades");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
