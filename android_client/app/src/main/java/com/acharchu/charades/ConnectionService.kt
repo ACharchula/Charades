c9a14f887d071cf8c31ec38f3e39cdef2f935125
ACharchula <antoni.charchula@gmail.com>
2019-04-18 22:33:43 +0200

    working with server refactor

--- android_client/app/src/main/java/com/acharchu/charades/ConnectionService.kt
+++ android_client/app/src/main/java/com/acharchu/charades/ConnectionService.kt
@@ -3,12 +3,13 @@ package com.acharchu.charades
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
+import java.lang.StringBuilder
 import java.net.Socket
 
 class ConnectionService {
 
     private val serverHost = "10.0.2.2" //has to be changed if u want to run on real smartphone
-    private val serverPort = 45454
+    private val serverPort = 44444
     private var socket: Socket? = null
     private var output: PrintWriter? = null
     private var input: BufferedReader? = null
@@ -19,13 +20,13 @@ class ConnectionService {
 
     var status = State.DISCONNECTED
 
-    private val id : Int = (0..100).random()
+    private val id : Int = (10..99).random()
 
     fun connectToServer() {
         try {
             socket = Socket(serverHost, serverPort)
 
-            output = PrintWriter(socket!!.getOutputStream(), true)
+            output = PrintWriter(socket!!.getOutputStream(), false)
             input = BufferedReader(InputStreamReader(socket?.getInputStream()))
 
             hello()
@@ -36,24 +37,45 @@ class ConnectionService {
     }
 
     private fun send(msg : String) {
-        output?.println(msg)
+        //output?.write(msg)
+        output?.print(msg)
+        output?.flush()
+        //output?.write(msg)
     }
 
 
     private fun hello() {
-        send("HELLO\nAndroidClient$id\n")
-//        if (get() != "WELCOME")
-//            throw java.lang.Exception()
+        send("HELLO_SERVER0015AndroidClient$id")
+        read(16)
     }
 
     fun sendMessage(msg : String) {
-        send("SEND_MESSAGE\n${msg.length}\n$msg")
+        send("SEND_MESSAGE${prepareMessageLength(msg.length)}$msg")
+    }
+
+    fun prepareMessageLength(length:Int) : String {
+        val builder = StringBuilder()
+
+        builder.append(length)
+
+        if(length < 10)
+            return "000$length"
+        else if(length < 100)
+            return "00$length"
+        else if(length < 1000)
+            return "0$length"
+        else if (length < 10000)
+            return length.toString()
+
+        return ""
+        //out of bound error
+
     }
 
     fun getMessage() : String? {
 
         val usernameAndMessageLength = read(BYTES_TO_READ_LENGTH)
-        val usernameAndMessage = read(usernameAndMessageLength.toInt()+1).split('\n')
+        val usernameAndMessage = read(usernameAndMessageLength.toInt()).split('\n')
 
         return "${usernameAndMessage[0]}: ${usernameAndMessage[1]}"
     }
